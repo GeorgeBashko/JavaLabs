c5eb3b74d700ff7cd02caa0324e9ab452c906481
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-10-26 22:31:57 +0300

    Add files via upload

--- untitled/Window.java
+++ untitled/Window.java
@@ -1,53 +1,47 @@
 import javax.swing.*;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
-import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 
 public class Window extends JFrame {
-    private final JLabel label = new JLabel();
+    private final JButton button1 = new JButton("No");
+    private final JButton button2 = new JButton("Yes");
+    private final JLabel label = new JLabel("Are you gay?");
+    private final JLabel answer = new JLabel("");
     Window(String s)
     {
         super(s);
+        setSize(600,600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        add(label,BorderLayout.SOUTH);
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-        add(panel,BorderLayout.CENTER);
-        JButton button = new JButton();
-        panel.add(button);
-        panel.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e)
-            {
-                setStatus(e.getX(),e.getY());
-                button.setBounds(e.getX(),e.getY(),20,20);
-            }
-        });
-        panel.addMouseMotionListener(new MyMotionAdapter() {
+        label.setBounds(200,50,100,50);
+        answer.setBounds(200,150,100,50);
+        answer.setVisible(false);
+        add(answer);
+        add(label);
+        setLayout(null);
+        button1.setBounds(100,100,100,40);
+        button2.setBounds(300,100,100,40);
+        add(button1);
+        add(button2);
+        button2.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseDragged(MouseEvent e) {
-
+            public void mouseClicked(MouseEvent e) {
+                answer.setText("C'est la vie");
+                answer.setVisible(true);
+                button2.setEnabled(false);
             }
-
+        });
+        button1.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseMoved(MouseEvent e) {
-
+            public void mouseEntered(MouseEvent e) {
+                int x = (int)(Math.random() * 450);
+                int y = (int)(Math.random() * 300) + 150;
+                if(button1.getX() + 50 >= x || button1.getY() + 50 >= y || button1.getY() - 50 <= y || button1.getX() <= x) {
+                    y += 50;
+                    x += 50;
+                }
+                    button1.setLocation(x,y);
             }
         });
-        button.add(new MouseMotionListener() {
-            public void mouseMoved(MouseEvent e)
-            {
-                setStatus(button.getX(),button.getY());
 
-            }
-        });
-    }
-    void setStatus(int x,int y)
-    {
-        label.setText("x: " + x + "y: " + y);
     }
 }
