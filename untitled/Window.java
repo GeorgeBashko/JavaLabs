86bdd97ee45e7987b52cbe1b5249c4b97e843f53
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-10-28 16:43:37 +0300

    Update Window.java

--- untitled/Window.java
+++ untitled/Window.java
@@ -3,17 +3,17 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Window extends JFrame {
-    private final JButton button1 = new JButton("No");
-    private final JButton button2 = new JButton("Yes");
-    private final JLabel label = new JLabel("Are you gay?");
+    private final JButton button2 = new JButton("Да");
+    private final JButton button1 = new JButton("Да,но...");
+    private final JLabel label = new JLabel("Вы довольны своей стипендией?");
     private final JLabel answer = new JLabel("");
     Window(String s)
     {
         super(s);
-        setSize(600,600);
+        setSize(650,600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        label.setBounds(200,50,100,50);
-        answer.setBounds(200,150,100,50);
+        label.setBounds(200,50,200,50);
+        answer.setBounds(200,150,200,50);
         answer.setVisible(false);
         add(answer);
         add(label);
@@ -25,7 +25,7 @@ public class Window extends JFrame {
         button2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                answer.setText("C'est la vie");
+                answer.setText("Это правильный ответ!");
                 answer.setVisible(true);
                 button2.setEnabled(false);
             }
@@ -33,13 +33,18 @@ public class Window extends JFrame {
         button1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                int x = (int)(Math.random() * 450);
-                int y = (int)(Math.random() * 300) + 150;
-                if(button1.getX() + 50 >= x || button1.getY() + 50 >= y || button1.getY() - 50 <= y || button1.getX() <= x) {
+                int x = (int)(Math.random() * 500);
+                int y = (int)(Math.random() * 320) + 150;
+                if(button1.getX() + 20 >= x || button1.getY() + 20 >= y || button1.getY() - 20 <= y || button1.getX() - 20 <= x) {
                     y += 50;
                     x += 50;
                 }
                     button1.setLocation(x,y);
+            }
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                answer.setText("Учитесь лучше!");
+                answer.setVisible(true);
             }
         });
 
